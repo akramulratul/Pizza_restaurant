b73754fcf1986ba5522b3161273eed73ee5a3fd9
Akramul Ratul <akramulratul@gmail.com>
2023-01-26 22:25:45 +0600

    cart

--- component/PizzaList.jsx
+++ component/PizzaList.jsx
@@ -1,7 +1,8 @@
 import styles from "../styles/PizzaList.module.css";
 import PizzaCard from "./PizzaCard";
 
-const PizzaList = () => {
+const PizzaList = ({ pizzaList }) => {
+  console.log(pizzaList);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
@@ -10,14 +11,10 @@ const PizzaList = () => {
         obcaecati atque dolores magni, assumenda incidunt.
       </p>
       <div className={styles.wrapper}>
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
+        {pizzaList.map((pizza) => (
+          // console.log(pizza)
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import axios from "axios";
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Featured from "../component/Featured";
 import PizzaList from "../component/PizzaList";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ pizzaList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +19,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList />
+      <PizzaList pizzaList={pizzaList} />
     </div>
   );
 }
+
+export const getServerSideProps = async()=>{
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props:{
+      pizzaList:res.data,
+    }
+  }
+}
+
+
+
+// const res = await axios.get("http://localhost:3000/api/products");
+// return {
+//   props: {
+//     pizzaList: res.data,
+//     admin,
+//   },
+// };
+//};
